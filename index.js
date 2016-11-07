08ca379329afb65af12efd26d9fd59122f3ee7b5
FIlburtTurtle <rancheta@berkeley.edu>
2016-11-07 13:01:30 -0800

    first commit - basic project

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 9393
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+
+app.get('/', function(req, res) {
+    res.sendFile('./public/index.html')
+})
+
+app.listen(port)
+console.log('Server is Up and Running at Port : ' + port)
