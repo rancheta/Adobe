21ef1ce438125e7e03944a766eb6d485f895ac09
FIlburtTurtle <rancheta@berkeley.edu>
2016-11-07 15:42:43 -0800

    collapse animation

--- public/scripts/carousel.js
+++ public/scripts/carousel.js
@@ -3,6 +3,7 @@
 var carouselHolder = document.getElementById('carouselHolder')
 var imagesArray = []
 var currentOffset = 0
+var totalCarouselWidth = 0;
 
 function createImage(imageObject) {
 	var url = 'https://farm' + imageObject.farm + '.staticflickr.com/' + imageObject.server + 
@@ -15,7 +16,6 @@ function createImage(imageObject) {
 }
 
 function buildCarousel(photoArray) {
-	var totalCarouselWidth = 0
 	if (photoArray.length > 30) {
 		photoArray.splice(31, photoArray.length - 31)
 	};
@@ -23,30 +23,33 @@ function buildCarousel(photoArray) {
 		var previousImg = i > 0 ? createImage(photoArray[i - 1]) : null
 		var img = createImage(photoArray[i])
 		img.style.left = 0 + 'px'
-		if (previousImg) {
-			totalCarouselWidth = previousImg.width + totalCarouselWidth
-			img.style.left = totalCarouselWidth + 'px'
-		}
 		imagesArray.push(img)
 		carouselHolder.appendChild(img) 
 	};
-	currentOffset = totalCarouselWidth
 }
 
-function getCurrentPosition(transformString) {
+//Animation to collapse carousel after onload and move to center of carousel
+function collapseCarouselOnLoad() {
+	for (var i = 1; i < imagesArray.length; i++) {
+		totalCarouselWidth = imagesArray[i - 1].width + totalCarouselWidth
+		imagesArray[i].style.left = totalCarouselWidth + 'px'
+	};
+}
+
+function getTranslatePosition(transformString) {
 	return Number(transformString.replace('translateX(', '').replace('px)', ''))
 }
 
 function rotateCarouselCounterClockwise() {
 	for (var i = 0; i < imagesArray.length; i++) {
-		var currentPosition = getCurrentPosition(imagesArray[i].style.transform)
+		var currentPosition = getTranslatePosition(imagesArray[i].style.transform)
 		console.log(imagesArray[i].style.transform, currentPosition)
 		imagesArray[i].style.transform = 'translateX(' + (currentPosition + 300) + 'px)'
 	};
 }
 function rotateCarouselClockwise() {
 	for (var i = 0; i < imagesArray.length; i++) {
-		var currentPosition = getCurrentPosition(imagesArray[i].style.transform)
+		var currentPosition = getTranslatePosition(imagesArray[i].style.transform)
 		console.log(imagesArray[i].style.transform, currentPosition)
 		imagesArray[i].style.transform = 'translateX(' + (currentPosition - 300) + 'px)'
 	};
@@ -59,4 +62,5 @@ var flickrUrl = 'https://api.flickr.com/services/rest/?method=flickr.photos.sear
 httpGetUrl(flickrUrl, function(data) {
 	console.log(JSON.parse(data))
 	buildCarousel(JSON.parse(data).photos.photo)
+	setTimeout(collapseCarouselOnLoad(), 500)
 })
